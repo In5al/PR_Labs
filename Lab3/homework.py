66dec81a7aaa56c123374dd993c919cfb3d29580
Phoenix <90285788+In5al@users.noreply.github.com>
2023-09-28 23:34:28 +0300

    Update homework.py

--- Lab3/homework.py
+++ Lab3/homework.py
@@ -1,1 +1,45 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+
+def extract_product_details(url):
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        title = soup.find("header", class_="adPage__header").text.strip()
+        
+        price_element = soup.find("span", class_="adPage__content__price-feature__prices__price__value")
+        currency_element = soup.find("span", class_="adPage__content__price-feature__prices__price__currency")
+        
+        price = price_element.text.strip() if price_element else None
+        currency = currency_element.text.strip() if currency_element else None
+
+        seller = soup.find("dl", class_="adPage__aside__stats__owner").text.strip()
+        description = soup.find("div", class_="adPage__content__description grid_18").text.strip()
+        characteristics = soup.find("div", class_="adPage__content__features__col grid_9 suffix_1").text.strip()
+        region = soup.find("dl", class_="adPage__content__region grid_18").text.strip()
+
+        product_details = {
+            "Title": title,
+            "Price": f"{price} {currency}" if price and currency else None,
+            "Seller": seller,
+            "Description": description,
+            "Characteristics": characteristics,
+            "Region": region,
+        }
+
+        return product_details
+
+    else:
+        print(f"Failed to retrieve the product page. Status code: {response.status_code}")
+        return None
+
+url = "https://999.md/ro/84212119"
+details = extract_product_details(url)
+
+if details:
+    json_details = json.dumps(details, indent=4, ensure_ascii=False)
+    print(json_details)
 
